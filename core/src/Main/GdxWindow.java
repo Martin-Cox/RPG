378b3f6ec08bfa6167a8b8f0f557223457dac6fe
Martin Stephen Cox <admin@martinstephencox.com>
2015-05-13 21:09:30 +0100

    Creating a status effect with an element now correctly parses element label

--- core/src/Main/GdxWindow.java
+++ core/src/Main/GdxWindow.java
@@ -2,23 +2,24 @@ package Main;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GdxWindow extends ApplicationAdapter {
-	SpriteBatch batch;
-	
-	@Override
-	public void create () {
+	private SpriteBatch batch;
+	private BitmapFont font;
+
+	public void create() {
 		batch = new SpriteBatch();
+		font = new BitmapFont(Gdx.files.internal("coval.fnt"));
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+
 		batch.begin();
+		font.draw(batch, "Hello World!", 100, 400);
 		batch.end();
 	}
+
 }
