4ced5d5363a26e6891fcb56dfa0a63f117625b76
Martin Stephen Cox <admin@martinstephencox.com>
2015-05-25 17:29:04 +0100

    Demonstration of simple user input on Android phone

--- core/src/main/GdxWindow.java
+++ core/src/main/GdxWindow.java
@@ -2,6 +2,8 @@ package main;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -13,22 +15,24 @@ public class GdxWindow extends ApplicationAdapter {
 	public void create() {
 		batch = new SpriteBatch();
 		font = new BitmapFont(Gdx.files.internal("coval.fnt"));
+		font2 = new BitmapFont(Gdx.files.internal("coval.fnt"));
 	}
 
 	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 
 		batch.begin();
+		batch.setColor(Color.BLUE);
 		font.draw(batch, "Hello World! Test custom font :)", 100, 400);
 		batch.end();
 
-		/*if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+
+		if(Gdx.input.isTouched() || Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
 			batch.begin();
 			font.dispose();
-			font2 = new BitmapFont(Gdx.files.internal("coval.fnt"));
-			font2.draw(batch, "You are pressing space!", 100, 400);
+			font2.draw(batch, "You are pressing left mouse/touching the screen", 100, 400);
 			batch.end();
-		}*/
+		}
 	}
 
 }
