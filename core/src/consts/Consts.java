77169acb1a8a0df0ea509226dcdaad864374defd
Martin Stephen Cox <admin@martinstephencox.com>
2015-06-05 17:25:18 +0100

    Added Constants master reference list

---
+++ core/src/consts/Consts.java
@@ -0,0 +1,13 @@
+package consts;
+
+/**
+ * Created by Martin on 05/06/2015.
+ */
+public class Consts {
+
+    public static final int WINDOW_HEIGHT = 1080;
+    public static final int WINDOW_WIDTH = 1920;
+
+    public static final String WINDOW_TITLE = "Martin RPG";
+
+}
--- desktop/src/main/desktop/DesktopLauncher.java
+++ desktop/src/main/desktop/DesktopLauncher.java
@@ -1,16 +1,17 @@
 package main.desktop;
 
-		import main.RunGame_OLD;
-		import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-		import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-		import main.RPGWindow;
+import main.RunGame_OLD;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import main.RPGWindow;
+import consts.Consts;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "RPG";
-		config.width = 1920;
-		config.height = 1080;
+		config.title = Consts.WINDOW_TITLE;
+		config.width = Consts.WINDOW_WIDTH;
+		config.height = Consts.WINDOW_HEIGHT;
 		config.fullscreen = true;
 		config.resizable = false;
 		new LwjglApplication(new RPGWindow(), config);
